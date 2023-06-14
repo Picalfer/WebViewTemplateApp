e970ca50ea5500da63776bc2e932edc4769b5ba2
Arthur Kozhemyakin <trable228@icloud.com>
2023-06-14 17:20:31 +0300

    webView work, back button without exit from app done

--- app/src/main/java/com/template/MainActivity.kt
+++ app/src/main/java/com/template/MainActivity.kt
@@ -3,10 +3,13 @@ package com.template
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.webkit.WebView
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -26,5 +29,18 @@ class MainActivity : AppCompatActivity() {
             .addOnFailureListener { exception ->
                 Log.d("result_db", "get failed with ", exception)
             }
+
+        webView = findViewById(R.id.webView)
+        webView.webViewClient = MyWebViewClient()
+        // включаем поддержку JavaScript
+        webView.getSettings().setJavaScriptEnabled(true)
+        // указываем страницу загрузки
+        webView.loadUrl("https://www.google.com/search?q=ntcn&oq=ntcn&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIGCAEQRRg7MgYIAhBFGDvSAQ0xOTA0MDkxMGowajE1qAIAsAIA&sourceid=chrome&ie=UTF-8")
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        }
     }
 }
