4b7967849291344ebce74eca2bb22c165ec0c48c
Arthur Kozhemyakin <trable228@icloud.com>
2023-06-15 15:35:19 +0300

    cookies are saved on local storage

--- app/src/main/java/com/template/webview/MyWebViewClient.kt
+++ app/src/main/java/com/template/webview/MyWebViewClient.kt
@@ -1,5 +1,6 @@
 package com.template.webview
 
+import android.webkit.CookieManager
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
@@ -8,5 +9,8 @@ class MyWebViewClient : WebViewClient() {
     override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
         view?.loadUrl(request?.url.toString())
         return true
+    }
+    override fun onPageFinished(view: WebView, url: String) {
+        CookieManager.getInstance().flush()
     }
 }
