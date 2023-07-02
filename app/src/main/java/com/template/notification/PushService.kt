07811a35918e7f1f37e9fcbb2a748eb369ce7c55
Arthur Kozhemyakin <trable228@icloud.com>
2023-07-03 00:08:10 +0300

    что-то добавил чтобы попробовать пуши починить

--- app/src/main/java/com/template/notification/PushService.kt
+++ app/src/main/java/com/template/notification/PushService.kt
@@ -1,10 +1,22 @@
 package com.template.notification
 
+import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import com.template.Constants
 
 class PushService: FirebaseMessagingService() {
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+        Log.d(Constants.TEST, "Refreshed token: $token")
+    }
 
-    override fun onNewToken(newToken: String) {
-        super.onNewToken(newToken)
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+        Log.d(Constants.TEST, "From: ${message.from}")
+
+        if (message.data.isNotEmpty()) {
+            Log.d(Constants.TEST, "Message data payload: ${message.data}")
+        }
     }
 }
